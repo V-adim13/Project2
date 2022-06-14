95bb971c1601d7259f0cffb2cb1fe4a6b32d96fc
*V-adim13* <pogopogo21696@gmail.com>
2022-06-14 17:21:48 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 11_360;
+
+        int males = price / 20;
+        System.out.println("Бонусные милли " + males);
+
+    }
+}
+
